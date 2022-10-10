43374d8ea4d105f7f0564fcb99eeb3eb1f53cf87
e1b20111 <e1b20111@st.oit.ac.jp>
2022-10-10 14:52:21 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/z0769/keisan/keisan/KeisanApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0769.keisan.keisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeisanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
